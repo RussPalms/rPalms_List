3775090d9393640a1b9acd4b72c85fa595fd94c6
RussPalms <russb.palms@gmail.com>
2021-04-28 20:54:23 -0700

    added more functionality

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     # if the path match is empty, run the home function from views
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    # this is the path when a new search is activated
+    path('new_search', views.new_search, name='new_search')
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 # when this funcion is called, it renders the base.html in templates
 def home(request):
     return render(request, 'base.html')
+
+# this fuction returns the result of the search page when ther is a new search
+def new_search(request):
+    # this is pulling out the data from when the submit button is pressed
+    search = request.POST.get('search')
+    print(search)
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
