6b1bb93ea2f8155b048ec58dfc4e013f8368ca3b
Russell Brian Fulache Dugaduga Jale Palma <rpalm@Russells-MacBook-Pro.local>
2022-05-07 18:32:20 -0700

    Ready to heroku dis

--- rpalms_list/settings.py
+++ rpalms_list/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,4 +137,4 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # this is calling the heroku functionality in django
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
